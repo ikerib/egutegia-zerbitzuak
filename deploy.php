1d03aae3bd2cb3afc97446231e7491bfcf5cd65b
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2021-01-27 12:03:54 +0100

    fix deployer - natives

---
+++ deploy.php
@@ -0,0 +1,66 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony3.php';
+
+// Project name
+set('application', 'egutegia-zerbitzuak');
+
+// Project repository
+set('repository', 'git@github.com:ikerib/egutegia-zerbitzuak.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', ['app/config/parameters.yml']);
+add('shared_dirs', ['var/logs','var/cache','web/uploads']);
+
+// Writable dirs by web server
+add('writable_dirs', ['var','web/uploads']);
+set('allow_anonymous_stats', false);
+
+// Hosts
+
+host('172.28.64.61')
+    ->user( 'root')
+    ->set('branch', 'main')
+    ->set('deploy_path', '/var/www/egutegia');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+
+set('bin/yarn', function () {
+    return run('which yarn');
+});
+desc('Install Yarn packages');
+task('yarn:install', function () {
+    if (has('previous_release')) {
+        if (test('[ -d {{previous_release}}/node_modules ]')) {
+            run('cp -R {{previous_release}}/node_modules {{release_path}}');
+        }
+    }
+    run("cd {{release_path}} && {{bin/yarn}}");
+});
+
+desc('Build my assets');
+task('yarn:build', function () {
+    if (has('previous_release')) {
+        if (test('[ -d {{previous_release}}/node_modules ]')) {
+            run('cp -R {{previous_release}}/node_modules {{release_path}}');
+        }
+    }
+    run("cd {{release_path}} && {{bin/yarn}} build");
+});
+
+after( 'deploy:symlink', 'yarn:install' );
+after( 'yarn:install', 'yarn:build' );
